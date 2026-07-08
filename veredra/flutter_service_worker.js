a9c2236b3dd438d70132ba5f79bd1744c83149ba
Usuario <wicolly@gmail.com>
2026-07-08 18:41:56 -0300

    fix: update Veredra PWA build

--- veredra/flutter_service_worker.js
+++ veredra/flutter_service_worker.js
@@ -49,14 +49,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "36f9db6e6fb8b6e4308e8c24f225c131
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "de3516d66910d97680055d1a5b94a423",
+"flutter_bootstrap.js": "1bee382dc2d7aae201999b2c21694ec1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "4debe258e41d26928298b5e2691ae834",
 "/": "4debe258e41d26928298b5e2691ae834",
-"main.dart.js": "cf01c8514d34aced09c6be3be2159e4a",
+"main.dart.js": "7f8e3e181c8e5350b6ca0bbfad476c22",
 "manifest.json": "d375f46a12ef54161e4e4aad1438db03",
 "version.json": "e2c99f12171b28985208bf36d2c8a3aa"};
 // The application shell files that are downloaded before a service worker can
@@ -142,6 +142,9 @@ self.addEventListener("activate", function(event) {
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== 'GET') {
     return;
+  }
+  if (event.request.mode === 'navigate') {
+    return respondWithCachedIndex(event);
   }
   var origin = self.location.origin;
   var key = resourceKeyFromUrl(event.request.url);
